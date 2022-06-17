3dca82c05a74735fd6b9e0459b6a05021f538bc0
Casey O'Neil <caseyoneil@haleys-MacBook-Pro.local>
2022-06-17 07:00:55 -0700

    fixed model

--- sleepapp/user_data_api/models.py
+++ sleepapp/user_data_api/models.py
@@ -6,4 +6,7 @@ class sleepData(models.Model):
     age = models.IntegerField()
     date = models.DateField()
     hoursSlept= models.IntegerField()
-    routine = models.CharField(max_length=110)
+    routine = models.CharField(max_length=100)
+    sleepQuality = models.IntegerField()
+    username = models.CharField(max_length=20)
+    notes = models.CharField(max_length=500)
