390f79c09f8107bb91b8b8842153998db5926cad
Casey O'Neil <caseyoneil@haleys-MacBook-Pro.local>
2022-06-17 07:12:03 -0700

    set default values for notes and usernames

--- sleepapp/user_data_api/models.py
+++ sleepapp/user_data_api/models.py
@@ -8,5 +8,5 @@ class sleepData(models.Model):
     hoursSlept= models.IntegerField()
     routine = models.CharField(max_length=100)
     sleepQuality = models.IntegerField()
-    # username = models.CharField(max_length=20, default='')
-    # notes = models.CharField(max_length=500, default='')
+    username = models.CharField(max_length=20, default='')
+    notes = models.CharField(max_length=500, default='')
